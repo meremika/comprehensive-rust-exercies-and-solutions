1cb58aaa79625aed6ce54b74bcc89356b9b4aa83
Michael Kaiser <michael.kaiser@gfn.education>
2025-06-23 10:39:18 +0200

    Exercise: Collatz Sequence

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@ fn fib(n: u32) -> u32 {
     if n < 2 { n } else { fib(n - 2) + fib(n - 1) }
 }
 
+/// Determine the length of the collatz sequence beginning at `n`.
+fn collatz_length(mut n: i32) -> u32 {
+    let mut length = 1;
+    while n > 1 {
+        n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
+        length += 1;
+    }
+    length
+}
+
 fn main() {
-    let n = 20;
-    println!("fib({n}) = {}", fib(n));
+    println!("fib(20) = {}", fib(20));
+    println!("collatz_length(11) = {}", collatz_length(11)); // should be 15
 }
